f810b0d0d0a3a3e97291fb77a5eef6def25f2b94
Shweta yadav <shwetayadav403@gmail.com>
2019-05-24 07:05:18 +0530

    2019-05-22

--- realtors/models.py
+++ realtors/models.py
@@ -4,7 +4,7 @@ from datetime import datetime
 
 class Realtor(models.Model): 
     name = models.CharField(max_length=100)
-    photo = models.ImageField(upload_to='photos/%Y/%m/%d/'),
+    photo = models.ImageField(upload_to='photos/%Y/%m/%d/')
     description = models.TextField(blank=True)
     email = models.CharField(max_length=50)
     phone = models.CharField(max_length=50)
